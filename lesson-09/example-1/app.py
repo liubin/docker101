5687b8e9fd4b4f34a0b9397c62a3d1209e4bace4
bin liu <liubin0329@gmail.com>
2015-09-05 18:03:18 +0800

    格式化输出,解决错误

--- lesson-09/example-1/app.py
+++ lesson-09/example-1/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World! I have been seen %s times.Env is: %s' % (redis.get('hits'), os.environ['MODE'])
+    return 'Hello World! I have been seen %s times.\n' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
